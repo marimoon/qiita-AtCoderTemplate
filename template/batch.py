c255a63e7a6bd98979d614b25c961df9d9cc2a6c
marimo <43314030+marimoon@users.noreply.github.com>
2021-01-17 10:59:30 +0900

    Add files via upload

---
+++ template/batch.py
@@ -0,0 +1,14 @@
+﻿from main import main
+import os
+import time
+
+in_txt = [ val for val in os.listdir( "./" ) if (val[-3:] == "txt") and (val[:2] == "in") ]
+in_txt = sorted(in_txt)
+
+for (i, in_file) in enumerate(in_txt):
+    print("\n_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/")
+    cmd = "python main.py < {0}".format(in_file)
+    print(cmd)
+    st = time.time()
+    os.system( cmd )
+    print( "laptime: {0:.3f} sec".format( time.time() - st ) )
---
+++ template/main.py
@@ -0,0 +1,16 @@
+﻿def get_list(type=int):
+    return list(map(type, input().split()))
+
+def get_int():
+    return int(input())
+
+def get_str():
+    return input()
+
+
+def main():
+    X, Y = get_list(int)
+    print( "Yes" if abs(X-Y) < 3 else "No")
+
+if __name__ == "__main__":
+    main()
